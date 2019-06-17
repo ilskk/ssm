b82a00e171e350c8ee3c55c45f03625dffc79a25
kk_global <kk_global@126.com>
2019-06-17 23:31:48 +0800

    ssm-learn

--- ssm-spring-web/src/main/java/com/kk/dao/StudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.kk.dao;
-
-public interface StudentDao {
-
-    String getStudentById(Integer id);
-
-}
--- ssm-spring-web/src/main/java/com/kk/dao/StudentDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.kk.dao;
-
-
-public class StudentDaoImpl implements StudentDao {
-    @Override
-    public String getStudentById(Integer id) {
-        return  "1:张三:33";
-    }
-}
